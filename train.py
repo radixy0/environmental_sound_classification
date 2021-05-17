5ecc7ecc3af21e1c9fb8a77ef8fd7fa21a8a1e60
radixy0 <80622923+radixy0@users.noreply.github.com>
2021-05-17 13:08:35 +0200

    implemented tensorboard logging

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 import numpy as np
 import model_architecture
 import os
+import datetime
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = "2"
 from tensorflow import keras
 from tensorflow.keras.optimizers import SGD
-import matplotlib.pyplot as plt
 
 model_dir = "model/"
 x_path = "data/x_train.npy"
@@ -13,8 +13,10 @@ y_path = "data/y_train.npy"
 x_val_path = "data/x_val.npy"
 y_val_path = "data/y_val.npy"
 
+log_dir = "logs/fit/"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
 num_classes = 10
-learning_rate = 0.0001
+learning_rate = 0.001
 decay = 1e-6
 momentum = 0.9
 epochs = 250
@@ -62,38 +64,17 @@ def main():
     model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
     callbacks=[
-        keras.callbacks.EarlyStopping(patience=5, verbose=1)
+        keras.callbacks.EarlyStopping(patience=5, verbose=1),
+        keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
     ]
 
     history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(x_val, y_val),
-                        verbose=1, callbacks=callbacks)
+                        verbose=2, callbacks=callbacks)
 
     model_filename = "model.h5"
     model.save(model_dir+model_filename, include_optimizer=True)
 
-    print("\n\nAll done! Model and Diagrams saved to /model" + model_filename)
-
-    # summarize history for accuracy
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig("model/accuracy.jpg")
-    plt.clf()
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.savefig("model/loss.jpg")
-
-
-    #os.system("shutdown -s -t 30")
-
+    print("\n\nAll done! Model saved to /model" + model_filename)
 
 if __name__ == "__main__":
     main()
